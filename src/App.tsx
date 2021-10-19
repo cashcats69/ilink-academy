8e2e95cb29658c64f5366ad3ee45ea4fd4efdc66
Ilya Kislenko <62322166+cashcats69@users.noreply.github.com>
2021-10-19 15:22:40 +0700

    feat: check-lint

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p >bobobo</p>
+        <p >bobooqnwnoqwnonqwonqwobo</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +18,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a >
       </header>
     </div>
   );
